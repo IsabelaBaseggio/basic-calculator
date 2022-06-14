a58dafe81615f7974343c7a5ab9c0abfdde20937
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2022-06-14 17:55:19 -0300

    backSpace

--- script/script.js
+++ script/script.js
@@ -6,3 +6,10 @@ document.getElementById("clear").addEventListener("click", ()=>{
     display.value = " ";
 })
 
+// backSpace
+
+let backSpace = () => {
+    const num = display.value.slice(0, -1);
+    display.value = num;
+}
+
